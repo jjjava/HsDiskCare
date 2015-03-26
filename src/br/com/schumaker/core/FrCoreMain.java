00d8023df3ce9de3c03906e1067b25f8f28660a1
hudson <hudson@192.168.0.2>
2015-03-26 07:50:46 -0300

    Ajustes

--- src/br/com/schumaker/core/FrCoreMain.java
+++ src/br/com/schumaker/core/FrCoreMain.java
@@ -1,6 +1,7 @@
 package br.com.schumaker.core;
 
 import java.io.File;
+import javax.swing.JLabel;
 import javax.swing.JProgressBar;
 
 /**
@@ -10,9 +11,10 @@ import javax.swing.JProgressBar;
 public class FrCoreMain {
 
     private static final FrCoreMain INSTANCE = new FrCoreMain();
+    private final File file;
 
     private FrCoreMain() {
-
+        file = new File("/");
     }
 
     public static FrCoreMain getInstance() {
@@ -20,11 +22,10 @@ public class FrCoreMain {
     }
 
     public void setMacintoshHD(JProgressBar bar) {
-        File file = new File("/");
         System.out.println(file.getTotalSpace());
         double max = ((file.getTotalSpace() / 1024) / 1024) / 1024;
         double free = ((file.getFreeSpace() / 1024) / 1024) / 1024;
-
+        
         System.out.println("Total space (bytes): " + ((file.getTotalSpace() / 1024) / 1024) / 1024);
         System.out.println("Free space (bytes): " + ((file.getFreeSpace() / 1024) / 1024) / 1024);
         System.out.println("Usable space (bytes): " + ((file.getUsableSpace() / 1024) / 1024) / 1024);
@@ -37,5 +38,12 @@ public class FrCoreMain {
         bar.setMinimum(0);
         bar.setValue((int) use);
     }
+    
+    public void setTotalSpace(JLabel x) {
+        x.setText(x.getText() + " " + ((file.getTotalSpace() / 1024) / 1024) / 1024);
+    }
 
+    public void setFreeSpace(JLabel x) {
+        x.setText(x.getText() + " " + ((file.getFreeSpace() / 1024) / 1024) / 1024);
+    }
 }
--- src/br/com/schumaker/gfx/FrMain.java
+++ src/br/com/schumaker/gfx/FrMain.java
@@ -16,18 +16,28 @@ public class FrMain extends javax.swing.JFrame {
         initComponents();
         setLocationRelativeTo(null);
         FrCoreMain.getInstance().setMacintoshHD(jProgressBar1);
+        FrCoreMain.getInstance().setTotalSpace(jlTotalSpace);
+        FrCoreMain.getInstance().setFreeSpace(jlFreeSpace);
     }
+
+    
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
         jProgressBar1 = new javax.swing.JProgressBar();
+        jlTotalSpace = new javax.swing.JLabel();
+        jlFreeSpace = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("HsDiskCare");
 
         jProgressBar1.setStringPainted(true);
+
+        jlTotalSpace.setText("Total Space:");
+
+        jlFreeSpace.setText("Free Space:");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -35,14 +45,24 @@ public class FrMain extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jProgressBar1, javax.swing.GroupLayout.DEFAULT_SIZE, 388, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jProgressBar1, javax.swing.GroupLayout.DEFAULT_SIZE, 388, Short.MAX_VALUE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jlTotalSpace)
+                            .addComponent(jlFreeSpace))
+                        .addGap(0, 0, Short.MAX_VALUE)))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(82, 82, 82)
-                .addComponent(jProgressBar1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(14, 14, 14)
+                .addComponent(jlTotalSpace)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jlFreeSpace)
+                .addGap(30, 30, 30)
+                .addComponent(jProgressBar1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addContainerGap(55, Short.MAX_VALUE))
         );
 
@@ -52,5 +72,7 @@ public class FrMain extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JProgressBar jProgressBar1;
+    private javax.swing.JLabel jlFreeSpace;
+    private javax.swing.JLabel jlTotalSpace;
     // End of variables declaration//GEN-END:variables
 }
